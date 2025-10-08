089adad1cd1febae15cb58ac1bc0f814766a7a1f
H Kilroy <henry.kilroy@vtsd.com>
2025-10-08 13:27:15 -0400

    sp8der

---
+++ 1.1.5/main.py
@@ -0,0 +1,83 @@
+#   a115_robot_maze.py
+import turtle as trtl
+
+#----- maze and turtle config variables
+screen_h = 400
+screen_w = 420
+startx = -100
+starty = -100
+turtle_scale = 1.5
+
+#------ robot commands
+def move():
+  robot.dot(10)
+  robot.fd(50)
+
+def turn_left():
+  robot.speed(0)
+  robot.lt(90)
+  robot.speed(2)
+
+#----- init screen
+wn = trtl.Screen()
+wn.setup(width=screen_w, height=screen_h)
+robot_image = "robot.gif"
+wn.addshape(robot_image)
+
+#----- init robot
+robot = trtl.Turtle(shape=robot_image)
+robot.hideturtle()
+robot.color("darkorchid")
+robot.pencolor("darkorchid")
+robot.penup()
+robot.setheading(90)
+robot.turtlesize(turtle_scale, turtle_scale)
+robot.goto(startx, starty)
+robot.speed(2)
+robot.showturtle()
+
+#---- TODO: change maze here
+wn.bgpic("maze3.png") # other file names should be maze2.png, maze3.png
+
+#---- TODO: begin robot movement here
+# move robot forward with move()
+# turn robot left with turn_left()
+# sample for loop:
+
+'''
+#MAZE 1 SOLUTION#
+
+for x in range(4):
+    move()
+for y in range(3):
+    turn_left()
+for z in range(4):
+    move()
+'''
+
+'''
+##MAZE 2 SOLUTION##
+
+for x in range(3):
+    move()
+for y in range(3):
+    turn_left()
+for z in range(2):
+    move()
+'''
+
+'''
+###MAZE 3 SOLUTION###
+for q in range(2):
+    move()
+    for x in range(3):
+        turn_left()
+    for y in range(2):
+        move()
+    turn_left()
+    move()
+'''
+
+#---- end robot movement 
+
+wn.mainloop()
