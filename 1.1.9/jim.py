550859c3d85954581dd3fb16d1fb4f1276cfa241
H Kilroy <henry.kilroy@vtsd.com>
2025-11-20 13:52:34 -0500

    lapotujbirfg

--- 1.1.9/jim.py
+++ 1.1.9/jim.py
@@ -11,6 +11,7 @@ for tut in range(2):
     jim.forward(150)
     jim.right(90)
 
+jim.color("purple")
 jim.pensize(25)
 jim.left(120)
 jim.circle(50, 180)
@@ -22,6 +23,9 @@ jim.pendown()
 jim.circle(50, -180)
 jim.left(180)
 jim.circle(50, 180)
+jim.color("violet")
+jim.penup()
+jim.teleport(0,0)
 
 wn = trtl.Screen()
 wn.mainloop()
