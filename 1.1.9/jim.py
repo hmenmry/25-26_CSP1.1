5458b41a525d491f8d281d1c1355302e53d91398
H Kilroy <henry.kilroy@vtsd.com>
2025-11-21 13:10:47 -0500

    lapotujbirfg

--- 1.1.9/jim.py
+++ 1.1.9/jim.py
@@ -26,6 +26,31 @@ jim.circle(50, 180)
 jim.color("violet")
 jim.penup()
 jim.teleport(0,0)
+jim.setheading(0)
+jim.forward(50)
+jim.left(90)
+jim.forward(100)
+jim.left(180)
+jim.pendown()
+
+
+for head in range(6):
+    jim.circle(100,180)
+    jim.right(120)
+
+jim.teleport(0,-150)
+jim.setheading(0)
+jim.setheading(270)
+jim.forward(50)
+
+
+
+
+
+
+
+
+
 
 wn = trtl.Screen()
 wn.mainloop()
