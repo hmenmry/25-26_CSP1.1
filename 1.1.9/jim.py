b249b6cc2a16d1bbeec641d60ede273ceb1e31ab
H Kilroy <henry.kilroy@vtsd.com>
2025-11-25 11:40:52 -0500

    lapotujbirfg

--- 1.1.9/jim.py
+++ 1.1.9/jim.py
@@ -33,7 +33,12 @@ jim.forward(100)
 jim.left(180)
 jim.pendown()
 
-
+'''if jim.pencolor("pink"):
+    jim.pencolor("black")
+elif jim.pencolor("black"):
+    jim.pencolor("blue")
+else:
+    jim.pencolor("red")'''
 for head in range(6):
     jim.circle(100,180)
     jim.right(120)
@@ -42,7 +47,39 @@ jim.teleport(0,-150)
 jim.setheading(0)
 jim.setheading(270)
 jim.forward(50)
-#hbu2fe3grtegregerghrge
+jim.right(90)
+jim.forward(20)
+jim.left(90)
+jim.forward(10)
+jim.left(90)
+jim.forward(30)
+jim.left(90)
+jim.forward(50)
+jim.penup()
+jim.teleport(0,0)
+jim.setheading(0)
+jim.forward(300)
+jim.right(90)
+jim.forward(150)
+jim.pendown()
+jim.forward(50)
+jim.left(90)
+jim.forward(20)
+jim.right(90)
+jim.forward(10)
+jim.right(90)
+jim.forward(30)
+jim.right(90)
+jim.forward(50)
+
+jim.setheading(0)
+jim.teleport(0,0)
+jim.pencolor("black")
+for tut in range(2):
+    jim.forward(300)
+    jim.right(90)
+    jim.forward(150)
+    jim.right(90)
 
 
 
